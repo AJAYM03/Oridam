60b7c36f16dd0d11c0eccb9de33335de699952ef
Ajay Mukund A <ajayma9@gmail.com>
2026-07-27 23:01:31 +0530

    revert: permanently remove Google Photos API integration (March 2025 API lockdown confirmed)

--- web/src/app/api/auth/google/route.ts
+++ web/src/app/api/auth/google/route.ts
@@ -9,8 +9,7 @@ export async function GET() {
     prompt: 'consent',
     scope: [
       'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/photoslibrary.readonly'
+      'https://www.googleapis.com/auth/userinfo.email'
     ],
   });
 
